97edb4493f50e2a446c726e9f21a721313ac9b2a
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-27 05:38:32 +0800

    retrieve one

--- backend/postRoutes.js
+++ backend/postRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const database = require('./connect');
+const { ObjectId } = require('mongodb');
 
 let postRoutes = express.Router();
 
@@ -9,6 +10,18 @@ postRoutes.route("/posts").get(async (req, res) => {
     let data = await db.collection("posts").find({}).toArray();
 
     if (data.length > 0 ) {
+        res.json(data);
+    } else {
+        throw new Error("No data found");
+    }
+});
+
+// Retrieve One
+postRoutes.route("/posts/:id").get(async (req, res) => {
+    let db = database.getDb();
+    let data = await db.collection("posts").findOne({ _id: new ObjectId(req.params.id) });
+
+    if (Object.keys(data).length > 0) {
         res.json(data);
     } else {
         throw new Error("No data found");
