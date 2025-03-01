741fe17ab8a76d9f0f51889921ccf25f5c36c74a
Ed Cabiltes <edcabiltes@outlook.com>
2025-03-01 14:37:45 +0800

    create pages

---
+++ frontend/pages/CreateBlog.jsx
@@ -0,0 +1,5 @@
+export default function CreateBlog() {
+  return (
+    <div>CreateBlog</div>
+  )
+}
---
+++ frontend/pages/ReadBlog.jsx
@@ -0,0 +1,5 @@
+export default function ReadBlog() {
+  return (
+    <div>ReadBlog</div>
+  )
+}
