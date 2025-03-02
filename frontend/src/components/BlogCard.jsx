17d3da4a540079fd17ce38fbe90a0ce585d34964
Ed Cabiltes <edcabiltes@outlook.com>
2025-03-02 11:21:46 +0800

    add readblog page with dynamic routing

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom"
+
 export default function BlogCard({ post }) {
     let date = new Date(post.dateCreated)
     let stringDate = date.toString()
 
   return (
-    <div className="post">
+    <Link to={`/read-blog/${post._id}`} className="post">
         <h2>{post.title}</h2>
         <p>{post.description}</p>
         <p>{stringDate.slice(4, 15)}</p>
-    </div>
+    </Link>
   )
 }
--- frontend/src/pages/ReadBlog.jsx
+++ frontend/src/pages/ReadBlog.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { getPost } from "../api"
+
+
 export default function ReadBlog() {
+  const [post, setPost] = useState({})
+
+  let params = useParams()
+  const navigate = useNavigate()
+  let id = params.id
+
+  useEffect(() => {
+    async function loadPost() {
+      const data = await getPost(id)
+      let date = new Date(data.dateCreated)
+      data.dateCreated = date.toString()
+      setPost(data)
+    }
+
+    loadPost()
+  }, [])
+
   return (
-    <div>ReadBlog</div>
+    <>
+      <button onClick={() => navigate(-1)}>Back</button>
+      <h1>{post.title}</h1>
+      <h2>{post.description}</h2>
+      <h3>{post.dateCreated?.slice(4, 15)}</h3>
+      <p>{post.content}</p>
+    </>
   )
 }
