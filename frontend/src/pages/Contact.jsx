b275eb0bc64ca36bec9763dca956133ee006bf82
Ed Cabiltes <edcabiltes@outlook.com>
2025-03-03 05:41:22 +0800

    add about and contact pages

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,21 @@
 export default function Contact() {
+  function handleSubmit() {}
+
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Contact Us</h1>
+      <p>Have questions or feedback? Feel free to reach out to us!</p>
+      <p>You can contact us via email at <a href="mailto:contact@example.com">contact@example.com</a>.</p>
+      <p>Alternatively, you can fill out the form below:</p>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input type="text" name="name" />
+        <label>Email:</label>
+        <input type="email"  name="email" />
+        <label>Message:</label>
+        <textarea name="message"></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
   )
 }
