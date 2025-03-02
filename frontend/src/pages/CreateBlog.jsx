3f7cd608f68af22069e77e4fa59228260306a7c4
Ed Cabiltes <edcabiltes@outlook.com>
2025-03-03 04:43:21 +0800

    add createblog page with form submission

--- frontend/src/pages/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react"
+import { createPost } from "../api"
+
 export default function CreateBlog() {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [content, setContent] = useState("")
+
+  async function handleSubmit() {
+    let submitObject = {
+      title: title,
+      description: description,
+      content: content,
+      author: null,
+      dateCreated: new Date()
+    }
+
+    await createPost(submitObject)
+  }
+
   return (
-    <div>CreateBlog</div>
+    <form onSubmit={handleSubmit}>
+      <label>Blog Post Title: </label>
+      <input onChange={(e) => setTitle(e.target.value)} maxLength={100} required name="title"/>
+      <label>Blog Description: </label>
+      <input onChange={(e) => setDescription(e.target.value)} maxLength={200} required name="description"/>
+      <label>Blog Content: </label>
+      <textarea onChange={(e) => setContent(e.target.value)} maxLength={5000} required name="content"/>
+      <button type="submit">Create Blog</button>
+    </form>
   )
 }
